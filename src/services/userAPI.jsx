99377a8598f461607e67bb9cc46a40fa3329ecec
AnantHansras <ananthansras.16@gmail.com>
2025-02-02 14:49:53 +0530

    naviagte on login path fixed

--- src/services/userAPI.jsx
+++ src/services/userAPI.jsx
@@ -90,7 +90,7 @@ export function login(email, password, navigate) {
       
       localStorage.setItem("token", JSON.stringify(response.data.token))
       localStorage.setItem("user", JSON.stringify(response.data.user))
-      navigate("/main/welcome")
+      navigate("/main/allusers")
 
     } catch (error) {
       console.log("LOGIN API ERROR............", error)
