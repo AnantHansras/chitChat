bd55f5a0dfe5285bb44d78c6dc29ec5c97b0aef8
AnantHansras <ananthansras.16@gmail.com>
2025-02-02 14:39:08 +0530

    stable 1.3

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "https://chitchat-backend-22lq.onrender.com";
 
 export const userEndpoints = {
     SENDOTP_API : BASE_URL+"/user/sendotp",
