ad49452b94665ef8b5fe0463c1009ebb74258cc3
AnantHansras <ananthansras.16@gmail.com>
2025-06-14 19:09:38 +0530

    jwtCheck middleware fixed

--- server/Routes/chat.js
+++ server/Routes/chat.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {jwtCheck} =require('../middlewares/auth')
+const jwtCheck =require('../middlewares/auth')
 
 const {accessChats,fetchChats,fetchAllGroups,
     createGroup,groupExit,addSelfToGroup,unSeenMessages
--- server/Routes/message.js
+++ server/Routes/message.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {jwtCheck} = require('../middlewares/auth')
+const jwtCheck = require('../middlewares/auth')
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const {allMessages,sendMessage,deleteMessage,reactToMessage} = require('../controllers/message');
