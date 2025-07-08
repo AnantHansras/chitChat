e83ed6f175748ab9e0c0a9ab69c601080be2fa8c
AnantHansras <ananthansras.16@gmail.com>
2025-07-08 16:12:40 +0530

    chatbanner imported

--- src/components/BotChatArea.jsx
+++ src/components/BotChatArea.jsx
@@ -14,6 +14,7 @@ import { useDispatch } from 'react-redux';
 import SelfBotmsg from './SelfBotmsg.jsx';
 import OtherBotmsg from './OtherBotmsg.jsx';
 import { Bot } from 'lucide-react';
+import ChatBanner from './ChatBanner.jsx';
 // import { sendToNovaAI } from '../services/aiAPI'; // You need to define this
 
 const BotChatArea = () => {
